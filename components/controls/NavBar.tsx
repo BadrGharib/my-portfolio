e21be70e6295533c23d71f26512cde153c4e0fa5
Badr Ghareab <badr.ghareab@g2k-group.com>
2022-12-10 23:11:18 +0200

    fet: :zap: hide menu after selection

--- components/controls/NavBar.tsx
+++ components/controls/NavBar.tsx
@@ -73,6 +73,7 @@ function NavBar ({
               return (
                 <li key={indx}>
                   <Link
+                    onClick={toggelNav}
                     href={item.href}
                     className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
                   >
